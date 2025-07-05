6f970fc91cbd51da102a02b4b9fe965166d10442
batman <luizarthurtesch@gmail.com>
2025-07-05 19:21:16 -0300

    avoid invalid input

--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,11 @@
+const allNotes = [
+  "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
+  "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
+  "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
+  "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
+  "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6"
+];
+
 function applyTheme(theme) {
     const body = document.body;
     const lcolumn = document.getElementById("lcolumn");
@@ -65,6 +73,21 @@ function updateNoteSelects(clefSelectId, lowSelectId, highSelectId) {
     populateNoteSelect(highSelectId, range.min, range.max);
 }
 
+function updateHighNoteOptions(lowSelectId, highSelectId, minSemitones = 3) {
+    const lowValue = document.getElementById(lowSelectId).value;
+    const highSelect = document.getElementById(highSelectId);
+    
+    const startIndex = allNotes.indexOf(lowValue);
+    highSelect.innerHTML = ''; // limpa
+
+    const options = allNotes.slice(startIndex + minSemitones);
+
+    options.forEach(note => {
+        const opt = new Option(note, note);
+        highSelect.add(opt);
+    });
+}
+
 const majorScales = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B'];
 const minorScales = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
 
@@ -92,8 +115,6 @@ function populateScaleSelect(selectId) {
 
 window.onload = () => {
     populateScaleSelect('scale');
-    updateNoteSelects("selectclef1", "select1l", "select1h");
-    updateNoteSelects("selectclef2", "select2l", "select2h");
 
     document.getElementById("selectclef1").addEventListener("change", () => {
         updateNoteSelects("selectclef1", "select1l", "select1h");
@@ -101,6 +122,20 @@ window.onload = () => {
     document.getElementById("selectclef2").addEventListener("change", () => {
         updateNoteSelects("selectclef2", "select2l", "select2h");
     });
+    document.getElementById("select1l").addEventListener("change", () => {
+    updateHighNoteOptions("select1l", "select1h");
+    });
+
+    document.getElementById("select2l").addEventListener("change", () => {
+        updateHighNoteOptions("select2l", "select2h");
+    });
+
+    updateNoteSelects("selectclef1", "select1l", "select1h");
+    updateNoteSelects("selectclef2", "select2l", "select2h");
+
+    updateHighNoteOptions("select1l", "select1h");
+    updateHighNoteOptions("select2l", "select2h");
+
 
     const savedTheme = localStorage.getItem("theme") || "light";
     applyTheme(savedTheme);
